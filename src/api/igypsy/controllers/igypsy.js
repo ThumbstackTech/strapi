e3ee77712119781fe82e7c0c9c21c5b009c1a9ba
Vagesh Vora <vageshvora12@gmail.com>
2022-01-18 12:39:08 +0530

    Client Data Structure Igypsy collection content

---
+++ src/api/igypsy/controllers/igypsy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  igypsy controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::igypsy.igypsy');
---
+++ src/api/igypsy/services/igypsy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * igypsy service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::igypsy.igypsy');
