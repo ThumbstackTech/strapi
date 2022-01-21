8ec736c5508ddf31018934176a77df511c1f82f0
Vagesh Vora <vageshvora12@gmail.com>
2022-01-21 12:28:37 +0530

    changes done

--- src/api/content/controllers/content.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  content controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::content.content');
