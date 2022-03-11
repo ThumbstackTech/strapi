2df58c9a8b43c486ca978ed0e864219fb85acc05
nitin <gangwarnitin111@gmail.com>
2022-03-11 11:44:03 +0530

    qr redirection improved

--- src/admin/extensions/components/TweetButton/index.js
+++ src/admin/extensions/components/TweetButton/index.js
@@ -11,7 +11,7 @@ const TweetButton = () => {
 
 
     const handleClick = () => {
-        const url = `https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=https://api.whatsapp.com/send?phone=918097364489&text=Hi,%20I%20want%20to%20know%20about%${encodeURIComponent(modifiedData.NameOfPlace)}%20id%20${encodeURIComponent(modifiedData.Content_code)}`
+        const url = `https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=https://api.whatsapp.com/send?phone=918097364489&text=Hi,%20I%20want%20to%20know%20about%20${encodeURIComponent(modifiedData.NameOfPlace)}%20id%20${encodeURIComponent(modifiedData.Content_code)}`
 
         window.open(url);
     }
