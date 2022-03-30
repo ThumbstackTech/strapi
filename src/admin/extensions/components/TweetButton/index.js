90d909ee850e6e1c48d1eca049caf61904e76ea6
nitin <gangwarnitin111@gmail.com>
2022-03-30 11:13:53 +0530

    extra field removed

--- src/admin/extensions/components/TweetButton/index.js
+++ src/admin/extensions/components/TweetButton/index.js
@@ -6,12 +6,11 @@ import { useIntl } from "react-intl";
 
 const TweetButton = () => {
     const { formatMessage } = useIntl();
-    const { modifiedData, layout, slug } = useCMEditViewDataManager();
-    const allowedTypes = ["final-igypsy-destination"]
+    const { modifiedData, slug } = useCMEditViewDataManager();
 
 
     const handleClick = () => {
-        const link = encodeURIComponent(`https://api.whatsapp.com/send?phone=918097364489&text=Hi%2C%20I%20want%20to%20know%20about%20${modifiedData.NameOfPlace}%20id%20${modifiedData.Content_code}`)
+        const link = encodeURIComponent(`https://api.whatsapp.com/send?phone=918097364489&text=Hi%2C%20I%20want%20to%20know%20about%20${(modifiedData.NameOfPlace)}%20id%20${modifiedData.Content_code}`)
         const url = `https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${link}
         `
 
